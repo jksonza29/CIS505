b2bd925d0617224dfa89aecc97bc2c3356aef630
JANZAL KARLO SONZA <jkgarciasonza@my365.bellevue.edu>
2022-03-17 18:05:39 -0700

    Added a Module_1 directory and completed HelloWorldApp

---
+++ Module_1/HelloWorldApp/SonzaHelloWorld.java
@@ -0,0 +1,9 @@
+public class SonzaHelloWorld{
+    String first = "Janzal Karlo";
+    String last = "Sonza";
+
+    public static void main(String[] args){
+        SonzaHelloWorld myName = new SonzaHelloWorld();
+        System.out.println("Hello World from " + myName.first + " " + myName.last);
+    }
+}
