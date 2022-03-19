d1d1e3818925bebbb295abd7c30eb6eeaddd134a
JANZAL KARLO SONZA <jkgarciasonza@my365.bellevue.edu>
2022-03-19 14:56:33 -0700

    Added TestFanApp.java and Fan.java files

---
+++ Module_1/FanApp/TestFanApp.java
@@ -0,0 +1,17 @@
+/*
+    Author: Janzal Karlo Sonza
+    Date: 03/19/2022
+    Purpose: Create fan objects and output the results 
+*/
+public class TestFanApp {
+    // main method
+    public static void main(String[] args) {
+        //instantiate two instances of the fan object in the fan class
+        //fan1 uses default constructor
+        //fan2 uses overloaded constructor
+
+        //output custom overridden toString method - this is actually how you do it
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
+    }
+}
